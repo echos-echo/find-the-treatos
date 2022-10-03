9cf02b52b5c28007a9b86ffa1f70e91aff540078
Eve Cho <e.young.echo@gmail.com>
2022-10-03 13:47:53 -0400

    server.js modified for error handling

--- server.js
+++ server.js
@@ -6,11 +6,18 @@ const DIST_DIR = path.join(__dirname, '/');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 app.use(express.static(DIST_DIR));
-app.get('/test', (req, res) => {
+app.use('/test', (req, res) => {
   res.send('hello world, testing');
 })
-app.get('*', (req, res) => {
+app.use('*', (req, res) => {
   res.sendFile(HTML_FILE);
+});
+
+app.use(function (err, req, res, next) {
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send("error: " + err.message);
 });
 
 const PORT = process.env.PORT || 8080;
