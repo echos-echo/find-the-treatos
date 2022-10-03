36a63b37d6dde12b3906ee262857884ff7ee3d96
Eve Cho <e.young.echo@gmail.com>
2022-10-03 13:38:46 -0400

    tester route added

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const DIST_DIR = path.join(__dirname, '/');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 app.use(express.static(DIST_DIR));
+app.get('/test', (req, res) => {
+  res.send('hello world, testing');
+})
 app.get('*', (req, res) => {
   res.sendFile(HTML_FILE);
 });
